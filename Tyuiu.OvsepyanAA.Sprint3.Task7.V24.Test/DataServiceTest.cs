5d823e4bf2f9f0b504efbd70649397aaffc38652
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-11-14 23:04:54 +0500

    Task7

---
+++ Tyuiu.OvsepyanAA.Sprint3.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.OvsepyanAA.Sprint3.Task7.V24.Lib;
+
+namespace Tyuiu.OvsepyanAA.Sprint3.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()        
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int endValue = 5;
+            double[] valueArray = new double[endValue - startValue + 1];
+            valueArray[0] = 7.83;
+            valueArray[1] = 6.22;
+            valueArray[2] = 6.36;
+            valueArray[3] = 6.96;
+            valueArray[4] = -0.52;
+            valueArray[5] = 0;
+            valueArray[6] = -3.3;
+            valueArray[7] = -5.53;
+            valueArray[8] = -6.25;
+            valueArray[9] = -6.63;
+            valueArray[10] = -8.24;
+            CollectionAssert.AreEqual(valueArray, ds.GetMassFunction(startValue, endValue));
+        }
+    }
+}
---
+++ Tyuiu.OvsepyanAA.Sprint3.Task7.V24/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.OvsepyanAA.Sprint3.Task7.V24.Lib;
+
+namespace Tyuiu.OvsepyanAA.Sprint3.Task7.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Овсепян А.А | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Овсепян Арам Андраникович | СМАРТб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит значение функции                    *");
+            Console.WriteLine("* ряда по формуле, при a=0,25                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("+-----------+------------+");
+            Console.WriteLine("|     X     |    F(x)    |");
+            Console.WriteLine("+-----------+------------+");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("|{0,6:d}     |   {1,5:f2}    |", startValue, res[i]);
+                startValue++;
+            }
+            Console.WriteLine("+-----------+------------+");
+            Console.ReadKey();
+        }
+    }
+}
